9d1c05eb00155f4d474e5c3770a64f75ca368a8a
Model Media <75977597+ModelMedia@users.noreply.github.com>
2022-10-07 17:00:20 +0530

    Update script.js

--- examples/script.js
+++ examples/script.js
@@ -7,8 +7,7 @@ circle(77, 77, 33, 'red');
 let box = new rect(222, 222, 150, 300, 'Blue');
 const vattam = new circle(33, 277, 50, 'green');
 const moola = new eqTriangle(55, 55, 450);
-const chithram1 = new DrawImg('1066.png', 0, 0, 4024, 1904, 0, 0, 100, 50);
 
-const newone = new eqTriangle(200, 200, 300);
+const newone = new eqTriangle(100, 50, 300);
 
-const newone1 = new eqTriangle(300, 300, 300, 'cyan');
+const newone1 = new eqTriangle(200, 100, 300, 'cyan');
