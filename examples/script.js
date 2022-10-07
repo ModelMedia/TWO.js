15cddb9764f739dae07f7de9c74cca7369dd21e1
Model Media <75977597+ModelMedia@users.noreply.github.com>
2022-10-07 16:57:34 +0530

    Add files via upload

---
+++ examples/script.js
@@ -0,0 +1,14 @@
+drawCanvas('black');
+centerCanvas();
+curvedCanvas('11px');
+canvasSize('75vw', '75vh');
+const writings = new text(33, 33, 'Hello World', '20px cursive', 'white', 1);
+circle(77, 77, 33, 'red');
+let box = new rect(222, 222, 150, 300, 'Blue');
+const vattam = new circle(33, 277, 50, 'green');
+const moola = new eqTriangle(55, 55, 450);
+const chithram1 = new DrawImg('1066.png', 0, 0, 4024, 1904, 0, 0, 100, 50);
+
+const newone = new eqTriangle(200, 200, 300);
+
+const newone1 = new eqTriangle(300, 300, 300, 'cyan');
